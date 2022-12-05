03208b3fb1067801f64c977ebb2d386d2e6d9253
Amber Cronin <amber@amber.vision>
2022-12-05 17:40:21 +0000

    [Day 5] Add working crate parser

--- 2022/5/5.py
+++ 2022/5/5.py
@@ -16,8 +16,27 @@ rearrangement_procedure = list(filter(None, input.split("\n\n")[1].split("\n")))
 # crates are separated by a space,
 # thus: a crate's identifier (it's letter) appears every (1 + (n*4)) chars
 
-crate_layers = crate_input.split("\n")[:-1]
-num_stacks = len(crate_input.split("\n").pop(-1).split())
 
-for n, layer in enumerate(crate_layers):
-    print(n, layer)
+def parse_crates(crate_string: str):
+    crate_layers = crate_input.split("\n")[:-1]
+    num_stacks = len(crate_input.split("\n").pop(-1).split())
+
+    stacks = []
+    for n in range(0, num_stacks):
+        stacks.append([])
+
+    for layer in crate_layers:
+        x = 0
+        while x < num_stacks:  # while there are still crates to parse in this layer
+            crate = layer[1 + (x * 4)]
+            if crate != " ":  # if there is actually a crate at this location
+                stacks[x].append(crate)
+            x += 1  # increment the stack counter
+
+    return stacks
+
+
+crates = parse_crates(crate_input)
+
+for n, stack in enumerate(crates):
+    print(n, stack)
