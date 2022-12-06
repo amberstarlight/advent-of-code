0367c4b83c72608848ed115f95fa46543c1abc24
Amber Cronin <amber@amber.vision>
2022-12-06 10:54:51 +0000

    [Day 6] Part 2 refactor

--- 2022/6/6.py
+++ 2022/6/6.py
@@ -4,16 +4,31 @@ from pathlib import Path
 
 input = Path("input.txt").read_text()
 
-queue = []
 
-for n, c in enumerate(input):
-    queue.append(c)
+def find_marker(input: str, index: int):
+    queue = []
+    marker = ""
+    marker_index = 0
 
-    if len(queue) > 4:
-        queue.pop(0)
+    for n, c in enumerate(input):
+        queue.append(c)
 
-        if len(set(queue)) == 4:
-            # set() returns no duplicate elements. if it returns 4 characters,
-            # we have found the marker at n+1 (zero indexing, remember?)
-            print("Marker found:", queue[-1], "\ncharacters processed:", n + 1)
-            break
+        if len(queue) > index:
+            queue.pop(0)
+
+            if len(set(queue)) == index:
+                # set() returns no duplicate elements.
+                # if it returns len(index) characters,
+                # we have found the marker at n+1
+                # (zero indexing, remember?)
+                marker = queue[-1]
+                marker_index = n + 1
+                break
+
+    return marker, marker_index
+
+
+part_one_answer = find_marker(input, 4)
+part_two_answer = find_marker(input, 14)
+
+print(part_one_answer, part_two_answer)
