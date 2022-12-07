7acd055a15f79893e3ad1e085c89e3fff6a03b67
Amber Cronin <amber@amber.vision>
2022-12-07 22:09:25 +0000

    Improve file class

--- 2022/7/7.py
+++ 2022/7/7.py
@@ -7,32 +7,52 @@ input = Path("test_input.txt").read_text()
 terminal_output_lines = list(filter(None, input.split("\n")))
 
 
-class Directory:
-    def __init__(self, name: str, contents: list = []):
-        self.name = name
-        self.contents = contents
-
-    def __repr__(self):
-        return "Directory: {0}, {1}".format(self.name, self.contents)
-
-
 class File:
-    def __init__(self, name: str, size: int):
+    def __init__(
+        self,
+        filetype: str,
+        name: str,
+        size: int,
+        parent_directory: str = "",
+        children: list = [],
+    ):
+        self.filetype = filetype
         self.name = name
         self.size = size
+        self.parent_directory = parent_directory
+        self.children = children
 
     def __repr__(self):
-        return "File: {0}, {1}".format(self.name, self.size)
+        return "{0}: {1}, size: {2}, {3}, {4}".format(
+            self.filetype.title(),
+            self.name,
+            self.size,
+            ("parent directory: " + self.parent_directory)
+            if self.parent_directory != ""
+            else "root",
+            self.children,
+        )
 
 
-filesystem = [Directory("/", [])]
-root = filesystem[0].contents
+filesystem = [
+    File(
+        filetype="directory",
+        name="/",
+        size=0,
+        parent_directory="",
+        children=[],
+    )
+]
+
+root = filesystem[0].children
 dir_depth = 0
 current_dir = ""
 prev_dir = ""
 
 for line in terminal_output_lines:
     start_char = line[0]
+
+    print("pwd =", current_dir)
 
     # we have 4 possible cases
     # $ cd <dir>
@@ -52,24 +72,40 @@ for line in terminal_output_lines:
                     current_dir = prev_dir
                 case "/":
                     dir_depth = 0
+                    prev_dir = current_dir
                     current_dir = cmd_args
                 case _:
                     dir_depth += 1
+                    prev_dir = current_dir
                     current_dir = cmd_args
 
         if command == "ls":
             # nothing to do,
             continue
 
-    if start_char.isdigit():  # file
+    if start_char.isdigit():
         file_info = line.split()
-        file = File(file_info[1], file_info[0])
+        file = File(
+            filetype="file",
+            name=file_info[1],
+            size=file_info[0],
+            parent_directory=current_dir,
+        )
         print(file)
 
     if start_char == "d":
         dir_name = line[4]
-        new_dir = Directory(dir_name, [])
+        new_dir = File(
+            filetype="directory",
+            name=dir_name,
+            size=0,
+            parent_directory=current_dir,
+        )
         print(new_dir)
-        print(filesystem)
+
+        if current_dir == "/":
+            root.append(new_dir)
+        else:
+            root.children.append()
 
 print(filesystem)
