528c804c04f9745e1687c57f59abb30f7a3f363d
Amber Cronin <amber@amber.vision>
2022-12-14 14:58:37 +0000

    Set up exterior tree visibility

--- 2022/8/8.py
+++ 2022/8/8.py
@@ -16,7 +16,14 @@ class Tree:
         self.height = int(height)
 
         if visibility == None:
-            self.visibility = {"top": None, "left": None, "right": None, "bottom": None}
+            self.visibility = {
+                "top": None,
+                "left": None,
+                "right": None,
+                "bottom": None,
+            }
+        else:
+            self.visibility = visibility
 
     def __repr__(self) -> str:
         return "Tree: {position}, height: {height}, {visibility}\n".format(
@@ -28,7 +35,7 @@ class Tree:
     def set_visibility(self, direction, visibility):
         self.visibility[direction] = visibility
 
-    def is_visible(self):
+    def is_visible(self) -> bool:
         if any(self.visibility.values()):
             return True
 
@@ -40,7 +47,21 @@ tree_grid = []
 for y, row in enumerate(tree_input):
     tree_grid.append([])
     for x, col in enumerate(row):
-        tree = Tree(x_pos=x, y_pos=y, height=col)
+        tree = Tree(
+            x_pos=x,
+            y_pos=y,
+            height=col,
+            visibility={
+                "top": True if y == 0 else None,
+                "left": True if x == 0 else None,
+                "right": True if x == (len(row) - 1) else None,
+                "bottom": True if y == (len(tree_input) - 1) else None,
+            },
+        )
         tree_grid[y].append(tree)
 
-print(tree_grid)
+
+for a in range(1, len(tree_grid) - 1):
+    for b in range(1, len(tree_grid[a]) - 1):
+        current_tree = tree_grid[a][b]
+        print(current_tree.position, current_tree.height, current_tree.visibility)
